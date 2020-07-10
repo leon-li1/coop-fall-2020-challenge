284ab366b771c95da8230048572bd30645e9726d
Leon Li <leonli369@gmail.com>
2020-07-10 14:23:59 -0400

    Done

--- solution_python.py
+++ solution_python.py
@@ -3,21 +3,34 @@ class EventSourcer():
 
     def __init__(self):
         self.value = 0
+        self.changes = []
+        self.undoChanges = []
 
     def add(self, num: int):
-        pass
+        self.value += num
+        self.changes.append(num)
 
     def subtract(self, num: int):
-        pass
+        self.add(-num)
 
     def undo(self):
-        pass
+        bulk_undo(1)
 
     def redo(self):
-        pass
+        bulk_redo(1)
 
     def bulk_undo(self, steps: int):
-        pass
-
+        # take the top steps of the redo stack and move them to the undo stack
+        undoVals = self.changes[-steps:]
+        undoVals.reverse()
+        self.value -= sum(undoVals)
+        self.changes = self.changes[:-steps]
+        self.undoChanges.extend(undoVals)
+        
     def bulk_redo(self, steps: int):
-        pass
+        # take the top steps of the undo stack and move them to the redo stack
+        redoVals = self.undoChanges[-steps:]
+        redoVals.reverse()
+        self.value += sum(redoVals)
+        self.undoChanges = self.undoChanges[:-steps]
+        self.changes.extend(redoVals)
